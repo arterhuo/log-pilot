6c69a7932431cba7ef907d2a2972c590d8da9932
ertao.xu <ertao.xu@leyantech.com>
2020-03-04 12:53:30 +0800

    remove drop_fields ["@metadata.filebeat", "@metadata.topic", "@metadata.type", "@metadata.version", "index", "prospector.type", "beat.hostname", "beat.name", "beat.version"]

--- build-image.sh
+++ build-image.sh
@@ -7,7 +7,9 @@ build()
 {
     echo -e "building image: log-pilot:latest\n"
 
-    docker build -t log-pilot:latest -f Dockerfile.$1 .
+    docker build -t log-pilot:latest -f Dockerfile.$1 . \
+        --build-arg HTTP_PROXY=http://127.0.0.1:1087 \
+	    --build-arg HTTPS_PROXY=http://127.0.0.1:1087
 }
 
 case $1 in
