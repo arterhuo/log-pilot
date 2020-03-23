254a4cbe47925084a4bf8a1250d59092ad52ebfc
ertao.xu <ertao.xu@leyantech.com>
2020-03-23 20:12:36 +0800

    upgrade 7.6.1 version

--- build-image.sh
+++ build-image.sh
@@ -7,9 +7,7 @@ build()
 {
     echo -e "building image: log-pilot:latest\n"
 
-    docker build -t log-pilot:latest -f Dockerfile.$1 . \
-        --build-arg HTTP_PROXY=http://127.0.0.1:1087 \
-	    --build-arg HTTPS_PROXY=http://127.0.0.1:1087
+    docker build -t log-pilot:latest -f Dockerfile.$1 . 
 }
 
 case $1 in
